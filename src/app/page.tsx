fa747a62b9a629b7d4e859455cff63e91f1dba79
mery <meryscarlet15@gmail.com>
2024-12-21 13:58:08 +0100

    feat add poligonhr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
-import { Metadata } from 'next'
-import styles from './page.module.scss'
-import Hero from '../modules/hero/hero'
-import Main from '../modules/layout/main/main'
+import { Metadata } from "next";
+import styles from "./page.module.scss";
+import Hero from "../modules/hero/hero";
+import Main from "../modules/layout/main/main";
+import { PolygonHr } from "@/components/hr/polygon-hr/polygon-hr";
 export const metadata: Metadata = {
-  title: 'Maria Amado | Full Stack Developer',
-  description: 'Full Stack Developer portfolio'
-}
+  title: "Maria Amado | Full Stack Developer",
+  description: "Full Stack Developer portfolio",
+};
 
 const Home = async () => {
   return (
     <Main className={styles.home__main}>
       <Hero />
+      <PolygonHr direction="topRight" />
     </Main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
