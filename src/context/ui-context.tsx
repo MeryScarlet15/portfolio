34c58fbb73cbb58ae5e57156e70072193dde7cc6
mery <meryscarlet15@gmail.com>
2024-12-23 10:31:36 +0100

    fix initial scroll issue

--- src/context/ui-context.tsx
+++ src/context/ui-context.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { createContext, useContext, useEffect, useState } from 'react'
+import React, { createContext, useContext, useState } from 'react'
 
 type TUIContext = {
   asideOpen: boolean
@@ -18,14 +18,6 @@ interface Props {
 
 export const UIContextProvider: React.FC<Props> = ({ children }: Props) => {
   const [asideOpen, setAsideOpen] = useState<boolean>(false)
-
-  useEffect(() => {
-    if ('scrollRestoration' in history) {
-      history.scrollRestoration = 'manual'
-    }
-
-    window.scrollTo(0, 0)
-  }, [])
 
   return (
     <UIContext.Provider
