95907436604cafa04b6fb6d129404e7e5895e8f0
mery <meryscarlet15@gmail.com>
2024-12-26 12:21:53 +0100

    fix: update footer year dynamically to reflect the current year

--- src/modules/layout/footer/footer.tsx
+++ src/modules/layout/footer/footer.tsx
@@ -9,7 +9,7 @@ export const Footer: React.FC = () => {
       <div className={styles.footer__container}>
         <div className={styles.footer__content}>
           <p className={styles.footer__text}>
-            2024 María Amado. Built with ❤️ in Spain.
+            {new Date().getFullYear()} María Amado. Built with ❤️ in Spain.
           </p>
           <div className={styles.footer__icons}>
             <Link
