424ea08b29181ab66e3781fb0aace033e41d14e2
mery <meryscarlet15@gmail.com>
2024-12-26 10:28:24 +0100

    refact Aluxion p

--- src/modules/experience/components/jobs/aluxion.tsx
+++ src/modules/experience/components/jobs/aluxion.tsx
@@ -2,20 +2,15 @@ import React from 'react'
 
 export const Aluxion = () => {
   return (
-    <>
-      <p>
-        I focused on fixing bugs and developing major features across multiple
-        projects using technologies like:
-      </p>
-
-      <ul>
-        <li>JavaScript & TypeScript</li>
-        <li>React.js & Next.js</li>
-        <li>Node.js & Express.js</li>
-        <li>HTML5 & CSS</li>
-        <li>jQuery & Styled Components</li>
-        <li>Git & Storybook</li>
-      </ul>
-    </>
+    <p>
+      I focused on fixing bugs and developing major features across multiple
+      projects, working with a wide range of technologies including{' '}
+      <span>JavaScript</span> and <span>TypeScript</span> for dynamic scripting,
+      <span>React.js</span> and <span>Next.js</span> for building modern user
+      interfaces, and <span>Node.js</span> with <span>Express.js</span> for
+      backend services. I also utilized HTML5,CSS, jQuery, and Styled Components
+      for front-end styling, along with Git for version control and{' '}
+      <span>Storybook</span> for component documentation and testing.
+    </p>
   )
 }
